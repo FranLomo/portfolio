4d138a03555d07c6f31d1652c445613d3f71e5d3
fralomo <francisco.lomo@estudiantes.uva.es>
2024-09-25 14:33:13 +0200

    Borradas carpetas innecesarias

--- app/aboutme/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AboutMePage() {
-  return (
-    <main className="min-h-screen">
-        <p>aboutme</p>
-    </main>
-  )
-}
--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProjectsPage() {
-  return (
-    <main className="min-h-screen">
-        <p>projects</p>
-    </main>
-  )
-}
